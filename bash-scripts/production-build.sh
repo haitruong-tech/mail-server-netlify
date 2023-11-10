7ad3e86d548d457c9db7c8ffff6200f64b20b003
Truong Quy Hai <haitruong.tech@gmail.com>
2023-11-10 16:43:22 +0700

    Echo pwd to test out how to put secret file to functions folder

--- bash-scripts/production-build.sh
+++ bash-scripts/production-build.sh
@@ -4,24 +4,26 @@ set -o allexport
 source ../.env
 set +o allexport
 
-path=../netlify/functions/portfolio-db.json
+echo [pwd]
 
-if [ -f $path ]; then
-  rm $path
-fi
+# path=../netlify/functions/portfolio-db.json
 
-echo $path
+# if [ -f $path ]; then
+#   rm $path
+# fi
 
-echo "{" >>$path
-echo "  \"type\": \"${TYPE}\"," >>$path
-echo "  \"project_id\": \"${PROJECT_ID}\"," >>$path
-echo "  \"private_key_id\": \"${PRIVATE_KEY_ID}\"," >>$path
-echo "  \"private_key\": \"${PRIVATE_KEY}\"," >>$path
-echo "  \"client_email\": \"${CLIENT_EMAIL}\"," >>$path
-echo "  \"client_id\": \"${CLIENT_ID}\"," >>$path
-echo "  \"auth_uri\": \"${AUTH_URI}\"," >>$path
-echo "  \"token_uri\": \"${TOKEN_URI}\"," >>$path
-echo "  \"auth_provider_x509_cert_url\": \"${AUTH_PROVIDER_X509_CERT_URL}\"," >>$path
-echo "  \"client_x509_cert_url\": \"${CLIENT_X509_CERT_URL}\"," >>$path
-echo "  \"universe_domain\": \"${UNIVERSE_DOMAIN}\"" >>$path
-echo "}" >>$path
+# echo $path
+
+# echo "{" >>$path
+# echo "  \"type\": \"${TYPE}\"," >>$path
+# echo "  \"project_id\": \"${PROJECT_ID}\"," >>$path
+# echo "  \"private_key_id\": \"${PRIVATE_KEY_ID}\"," >>$path
+# echo "  \"private_key\": \"${PRIVATE_KEY}\"," >>$path
+# echo "  \"client_email\": \"${CLIENT_EMAIL}\"," >>$path
+# echo "  \"client_id\": \"${CLIENT_ID}\"," >>$path
+# echo "  \"auth_uri\": \"${AUTH_URI}\"," >>$path
+# echo "  \"token_uri\": \"${TOKEN_URI}\"," >>$path
+# echo "  \"auth_provider_x509_cert_url\": \"${AUTH_PROVIDER_X509_CERT_URL}\"," >>$path
+# echo "  \"client_x509_cert_url\": \"${CLIENT_X509_CERT_URL}\"," >>$path
+# echo "  \"universe_domain\": \"${UNIVERSE_DOMAIN}\"" >>$path
+# echo "}" >>$path
