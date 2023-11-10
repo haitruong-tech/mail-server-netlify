1ec8a44aed6c06005a5bddc6e36095f19d31cc1d
Truong Quy Hai <haitruong.tech@gmail.com>
2023-11-10 16:02:06 +0700

    Get client IP to prevent DDOS

--- netlify/functions/mail-server.js
+++ netlify/functions/mail-server.js
@@ -11,14 +11,14 @@ import { sendMail } from "../../services/mail";
 const spreadsheetId = process.env.SPREAD_SHEET_ID;
 const sheetName = process.env.SHEET_NAME;
 
-async function addContact({ name, email, content }, auth, position) {
+async function addContact({ name, email, content, ip }, auth, position) {
   try {
     const updateResponse = await addRow({
       auth,
       range: `${sheetName}!A${position}:F${position}`,
       spreadsheetId,
-      // id, name, email, content, created_at
-      values: [[uuidv4(), name, email, content, new Date().toISOString()]],
+      // id, name, email, content, created_at, ip
+      values: [[uuidv4(), name, email, content, new Date().toISOString(), ip]],
     });
     return updateResponse.status;
   } catch (error) {
@@ -34,6 +34,7 @@ const headers = {
 
 export const handler = async (event, context) => {
   try {
+    const ip = event.headers["client-ip"];
     const auth = await getAuthToken();
     const response = await getSpreadSheetValues({
       spreadsheetId,
@@ -45,7 +46,7 @@ export const handler = async (event, context) => {
     date.setUTCHours(0, 0, 0, 0);
 
     const mails = response.data.values?.filter(
-      (mail) => new Date(mail[4]).getTime() > date.getTime()
+      (mail) => new Date(mail[4]).getTime() > date.getTime() && mail[5] === ip
     );
     if (mails.length >= 5) {
       return {
@@ -56,18 +57,17 @@ export const handler = async (event, context) => {
     }
 
     const contact = JSON.parse(event.body);
-    addContact({ ...contact }, auth, (response.data.values?.length ?? 0) + 1);
+    addContact(
+      { ...contact, ip },
+      auth,
+      (response.data.values?.length ?? 0) + 1
+    );
     sendMail({ ...contact });
 
     return {
       headers,
       statusCode: 200,
       body: JSON.stringify("I've received your mail"),
-      // headers: {
-      //   "Access-Control-Allow-Origin": "*",
-      //   "Access-Control-Allow-Headers": "Content-Type",
-      //   "Access-Control-Allow-Methods": "GET, POST",
-      // },
     };
   } catch (error) {
     console.error(error);
