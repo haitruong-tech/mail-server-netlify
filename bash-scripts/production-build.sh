92eec9a548d32f63fcb131f4416a885da670ccb6
Truong Quy Hai <haitruong.tech@gmail.com>
2023-11-10 16:57:01 +0700

    Fix production build script

--- bash-scripts/production-build.sh
+++ bash-scripts/production-build.sh
@@ -4,28 +4,24 @@ set -o allexport
 source ../.env
 set +o allexport
 
-echo `pwd`
-echo `ls ..`
-echo `ls`
+path=netlify/functions/portfolio-db.json
 
-# path=../netlify/functions/portfolio-db.json
+if [ -f $path ]; then
+  rm $path
+fi
 
-# if [ -f $path ]; then
-#   rm $path
-# fi
+echo $path
 
-# echo $path
-
-# echo "{" >>$path
-# echo "  \"type\": \"${TYPE}\"," >>$path
-# echo "  \"project_id\": \"${PROJECT_ID}\"," >>$path
-# echo "  \"private_key_id\": \"${PRIVATE_KEY_ID}\"," >>$path
-# echo "  \"private_key\": \"${PRIVATE_KEY}\"," >>$path
-# echo "  \"client_email\": \"${CLIENT_EMAIL}\"," >>$path
-# echo "  \"client_id\": \"${CLIENT_ID}\"," >>$path
-# echo "  \"auth_uri\": \"${AUTH_URI}\"," >>$path
-# echo "  \"token_uri\": \"${TOKEN_URI}\"," >>$path
-# echo "  \"auth_provider_x509_cert_url\": \"${AUTH_PROVIDER_X509_CERT_URL}\"," >>$path
-# echo "  \"client_x509_cert_url\": \"${CLIENT_X509_CERT_URL}\"," >>$path
-# echo "  \"universe_domain\": \"${UNIVERSE_DOMAIN}\"" >>$path
-# echo "}" >>$path
+echo "{" >>$path
+echo "  \"type\": \"${TYPE}\"," >>$path
+echo "  \"project_id\": \"${PROJECT_ID}\"," >>$path
+echo "  \"private_key_id\": \"${PRIVATE_KEY_ID}\"," >>$path
+echo "  \"private_key\": \"${PRIVATE_KEY}\"," >>$path
+echo "  \"client_email\": \"${CLIENT_EMAIL}\"," >>$path
+echo "  \"client_id\": \"${CLIENT_ID}\"," >>$path
+echo "  \"auth_uri\": \"${AUTH_URI}\"," >>$path
+echo "  \"token_uri\": \"${TOKEN_URI}\"," >>$path
+echo "  \"auth_provider_x509_cert_url\": \"${AUTH_PROVIDER_X509_CERT_URL}\"," >>$path
+echo "  \"client_x509_cert_url\": \"${CLIENT_X509_CERT_URL}\"," >>$path
+echo "  \"universe_domain\": \"${UNIVERSE_DOMAIN}\"" >>$path
+echo "}" >>$path
